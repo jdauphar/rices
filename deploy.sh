8e58ef7fc17c7b5084f4c05d0d0ae14f14e38312
Jack <jdauphar@kent.edu>
2020-11-21 03:55:05 +0000

    update deployment script to newer tmux

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 echo "copying"
 git clone https://github.com/technomancy/better-defaults.git
-cp .tmux ~/.tmux;
+cp .tmux.conf ~/.tmux.conf;
 cp init.el ~/.emacs.d/init.el;
 cp -r better-defaults ~/.emacs.d/.better-defaults
 echo "done"
